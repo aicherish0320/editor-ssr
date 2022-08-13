c6e6a1e60f4aace680bb8f9636d5dfddefd1475b
aicherish <aicherish0320@163.com>
2022-08-13 18:56:30 +0800

    模板引擎测试

---
+++ src/mock/work.js
@@ -0,0 +1,73 @@
+const work = {
+  _id: '62f4f0b398a41408fd76cdf2',
+  isTemplate: false,
+  copiedCount: 0,
+  status: 2,
+  channels: [
+    { name: '默认', id: 'S77Jm1' },
+    { name: '12', id: 'rJAk65' }
+  ],
+  title: '未命名作品',
+  desc: '未命名作品',
+  coverImg: 'https://static.imooc-lego.com/imooc-test/lIYArY.png',
+  user: '6231aa0987554208ff7d3fcd',
+  author: '13979325136',
+  uuid: 'sCDAE7',
+  createdAt: '2022-08-11T12:06:11.839Z',
+  updatedAt: '2022-08-13T10:48:47.635Z',
+  id: 3364,
+  __v: 0,
+  content: {
+    components: [
+      {
+        name: 'l-text',
+        props: {
+          text: '大标题',
+          fontSize: '30px',
+          fontFamily: '',
+          fontWeight: 'bold',
+          fontStyle: 'italic',
+          textDecoration: 'none',
+          lineHeight: '1',
+          textAlign: 'center',
+          color: '#fa541c',
+          backgroundColor: '#52c41a',
+          actionType: '',
+          url: '',
+          height: '57px',
+          width: '167.1666259765625px',
+          paddingLeft: '0px',
+          paddingRight: '0px',
+          paddingTop: '0px',
+          paddingBottom: '0px',
+          borderStyle: 'none',
+          borderColor: '#000',
+          borderWidth: '0',
+          borderRadius: '0',
+          boxShadow: '0 0 0 #000000',
+          opacity: 1,
+          position: 'absolute',
+          left: '99.66668701171875px',
+          top: '59px',
+          right: '0',
+          tag: 'h2'
+        },
+        id: 'd582bffc-0d10-4a5d-acac-df64cb6211ff',
+        layerName: '图层1',
+        isHidden: false,
+        isLocked: false
+      }
+    ],
+    props: {
+      backgroundColor: '#ffffff',
+      backgroundImage: '',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      height: '560px'
+    },
+    setting: {}
+  },
+  latestPublishAt: '2022-08-13T10:48:47.635Z'
+}
+
+module.exports = work
