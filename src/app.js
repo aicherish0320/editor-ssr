9163fffc2e9e89794ec42393558852f7bad4af63
aicherish <aicherish0320@163.com>
2022-08-13 18:55:20 +0800

    init project

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const Koa = require('koa')
+const views = require('koa-views')
+const index = require('./routes/index')
+const works = require('./routes/works')
+
+const app = new Koa()
+
+app.use(require('koa-static')(`${__dirname}/public`))
+
+app.use(
+  views(`${__dirname}/views`, {
+    extension: 'pug'
+  })
+)
+
+app.use(index.routes(), index.allowedMethods())
+app.use(works.routes(), works.allowedMethods())
+
+app.listen('3300', () => console.log('3300 port'))
