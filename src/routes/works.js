4e6db8bb9b8abba984ead3fc9b1bc35b92733af5
aicherish <aicherish0320@163.com>
2022-08-14 16:01:19 +0800

    事件绑定

--- src/routes/works.js
+++ src/routes/works.js
@@ -10,12 +10,28 @@ async function renderPage(ctx, work, pageType) {
   const bodyStyle = propsToStyle(props)
 
   const componentsHtml = await getLegoComponentsHtml(components)
+
+  // 组件的事件
+  const eventInfoList = []
+  components.forEach((component) => {
+    const { id, props: componentProps = {} } = component
+    const { actionType, url } = componentProps
+    if (actionType) {
+      eventInfoList.push({
+        id,
+        actionType,
+        url
+      })
+    }
+  })
+
   await ctx.renderWithAssets('work', {
     title,
     desc,
     bodyStyle,
     content: componentsHtml,
-    pageType
+    pageType,
+    eventInfoList: JSON.stringify(eventInfoList)
   })
 }
 
@@ -24,7 +40,7 @@ router.get('/ssr', async (ctx) => {
   //   // 后面要承载 Vue3 SSR 的结果 - html 标签
   //   content: `<div style="color: blue;">${JSON.stringify(work)}</div>`
   // })
-  await renderPage(ctx, work, 'template')
+  await renderPage(ctx, work, 'publish')
 })
 
 module.exports = router
